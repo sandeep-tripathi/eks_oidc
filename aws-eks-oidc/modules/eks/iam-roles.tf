36b58c7db85c4a8a68a8de2e7f365ef08b09a83b
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2024-08-10 13:49:24 +0200

    Update iam-roles.tf

---
+++ aws-eks-oidc/modules/eks/iam-roles.tf
@@ -0,0 +1,52 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role-${var.cluster_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+output "eks_cluster_role_arn" {
+  value = aws_iam_role.eks_cluster_role.arn
+}
+
+# IAM Role for Node Groups
+resource "aws_iam_role" "node_group_role" {
+  name = "eks-node-group-role-${var.cluster_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_group_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group_role.name
+}
+
+output "node_group_role_arn" {
+  value = aws_iam_role.node_group_role.arn
+}
