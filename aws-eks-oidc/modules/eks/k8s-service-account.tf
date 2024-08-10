89e5013f3c58f1a08bc0ac4ce8d3ce9c2edacfdf
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2024-08-10 14:15:50 +0200

    Update k8s-service-account.tf

--- aws-eks-oidc/modules/eks/k8s-service-account.tf
+++ aws-eks-oidc/modules/eks/k8s-service-account.tf
@@ -1,3 +1,7 @@
+##Creating service account
+## Enabling EKS OIDC provider
+## Creating  IAM Role for Service Accounts (IRSA)
+#####################################
 # Ensure the EKS OIDC provider is enabled
 data "aws_eks_cluster" "cluster" {
   name = aws_eks_cluster.dtg.name
@@ -6,13 +10,13 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.dtg.name
 }
-
+# Configure OIDC Provider in AWS
 resource "aws_iam_openid_connect_provider" "eks_oidc" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer]
   url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
-
+# IAM Role for Service Accounts (IRSA)
 resource "aws_iam_role" "sa_role" {
   name = "${var.cluster_name}-sa-role"
 
@@ -34,12 +38,12 @@ resource "aws_iam_role" "sa_role" {
     ]
   })
 }
-
+#
 resource "aws_iam_role_policy_attachment" "sa_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   role       = aws_iam_role.sa_role.name
 }
-
+#  Create the Kubernetes Service Account
 resource "kubernetes_service_account" "sa" {
   metadata {
     name      = var.service_account_name
